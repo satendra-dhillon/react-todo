77a22db82d30d1c059226a2dfc0c67933ba27b6f
satendra singh <satendra@meter.taxi>
2018-11-26 16:38:27 +0530

    add props to the component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Component } from 'react';
+import Table from './Table.js';
 
 
 class App extends Component {
     render(){
+        const characters = [
+            {
+                'name': 'Charlie',
+                'job': 'Janitor'
+            },
+            {
+                'name': 'Mac',
+                'job': 'Bouncer'
+            },
+            {
+                'name': 'Dee',
+                'job': 'Aspring actress'
+            },
+            {
+                'name': 'Dennis',
+                'job': 'Bartender'
+            }
+        ];
+
         return (
-            <div className= "App">
-                <h1>HEllo, React !</h1>
+            <div className="container">
+                <Table  characterData = {characters} />
             </div>
         );     
     }
---
+++ src/Table.js
@@ -0,0 +1,17 @@
+import  React, { Component } from 'react';
+import TableHeader from './TableHeader.js';
+import TableBody from './TableBody.js';
+
+class Table extends Component {
+    render (){
+        const {characterData } = this.props;
+        return(
+            <table>
+                <TableHeader />
+                <TableBody  characterData={characterData}/>
+            </table>  
+        )
+    }
+}
+
+export default Table;
